a86ceaa2df684448beb3d79aa9b11746e78cc47b
shiyanshirani <shiyan99s@gmail.com>
2021-08-30 23:11:53 +0530

    server.js, index.js, api(dir) dao(dir)

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express" 
+import cors from "cors"
+import restaurants from "./api/restaurants.route.js"
+
+// Creating app
+const app = express()
+
+// App's gonna use cors
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/restaurants", restaurants)
+app.use("*", (req, res) => res.status(404).json({error : " Not found"}))
+
+export default app
+
