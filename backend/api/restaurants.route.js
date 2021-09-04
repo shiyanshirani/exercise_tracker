fe2570a2ab2848e2b160ecb744a3150c8ce9cd01
shiyanshirani <shiyan99s@gmail.com>
2021-09-04 23:58:32 +0530

    routes added

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,6 +1,7 @@
 import express from "express"
 import RestaurantCtrl from "./restaurants.controller.js"
 
+
 const router = express.Router()
 
 router.route("/").get(RestaurantCtrl.apiGetRestaurants)
