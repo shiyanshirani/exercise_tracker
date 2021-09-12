81207ec46679f00a6f72a1547d0202e42dbfe3a8
shiyanshirani <shiyan99s@gmail.com>
2021-09-12 23:24:56 +0530

    exported app

--- backend/server.js
+++ backend/server.js
@@ -12,5 +12,5 @@ app.use(express.json())
 // app.use("/api/v1/restaurants", restaurants)
 // app.use("*", (req, res) => res.status(404).json({error : " Not found"}))
 
-// export default app
+export default app
 
